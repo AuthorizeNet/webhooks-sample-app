29690130f80f8384523353aa7882b3c95b9a0770
Saravanan <jsaravan@visa.com>
2018-06-03 21:38:17 -0700

    able to create a new end point

--- server.js
+++ server.js
@@ -13,23 +13,31 @@ const port = 8000;
 //});
 
 //app.use('/admin', admin);
+var headers = { "content-type": "application/json", "Authorization": "Basic " + "your encoded authentication string" }
 
 var eventRequestData = {
     url: 'https://apitest.authorize.net/rest/v1/eventtypes',
     method: 'GET',
-    headers: { "content-type": "application/json", "Authorization": "Basic " + "your encoded authentication string" },
+    headers: headers,
 
 };
 
 var webhooksRequestData = {
     url: 'https://apitest.authorize.net/rest/v1/webhooks',
     method: 'GET',
-    headers: { "content-type": "application/json", "Authorization": "Basic " + "your encoded authentication string" },
+    headers: headers,
+};
+
+var webhooksPostData = {
+    url: 'https://apitest.authorize.net/rest/v1/webhooks',
+    method: 'POST',
+    headers: headers,
+    json: true,
 };
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 var response_event = [
     { event_name: 'e1', webhook_id: 'a1' },
@@ -54,8 +62,6 @@ function getEvents(res) {
         //    //console.log(JSON.parse(body));
         //    console.log("Body: " + JSON.parse(body).status + " reason: " + JSON.parse(body).reason + " message: " + JSON.parse(body).message)
         //}
-        console.log("\nin get events\n")
-        console.log("geteventsfunc\n", eventList);
         res.send(eventList);
     });
     
@@ -98,6 +104,28 @@ app.get('/events', function (req, res) {
 
 app.get('/webhooks', function (req, res) {
     getWebhooks(res);
+});
+
+app.post('/webhooks', function (req, res) {
+    //postWebhooks(res);
+
+    //webhooksPostData = Object.assign(webhooksPostData, req.body);
+    console.log("this is req.body in server.js\n");
+    //console.log(req.body);
+    webhooksPostData.body = req.body;
+    console.log(webhooksPostData);
+    request(webhooksPostData, function (error, response, body) {
+        if (error) {
+            console.log("error occured: \n", error);
+            //res.write("Error");
+        }
+        else {
+            console.log(response.body);
+            //res.sendStatus(200);
+        }
+        
+    });
+
 });
 
 app.get('/webhooks/add', function (req, res) {
