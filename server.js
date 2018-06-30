35f06462fb796ea5bf02e1c65f9261543839d4d1
Saravanan <jsaravan@visa.com>
2018-06-29 17:36:06 -0700

    placed credentials in config file, added error handling and refractored code

--- server.js
+++ server.js
@@ -1,119 +1,89 @@
-var http = require('http');
-var express = require('express')
-var app = express();
-//var admin = express();
-var request = require('request');
-var bodyParser = require('body-parser');
+const express = require("express");
+const app = express();
+const request = require("request");
+const bodyParser = require("body-parser");
 
-const port = 8000;
+const http = require("http");
+const socketIo = require("socket.io");
+const server = http.Server(app);
+const cors = require("cors");
+const io = socketIo(server);
 
-//admin.get('/', function (req, res) {
-//    console.log(admin.mountpath); // /admin
-//    res.send('Admin Homepage');
-//});
+const config = require("./config");
 
-//app.use('/admin', admin);
-var headers = { "content-type": "application/json", "Authorization": "Basic " + "your encoded authentication string" }
+const encodedString = Buffer.from(config.apiLoginId + ":" +
+                                  config.transactionKey).toString("base64");
 
-var eventRequestData = {
-    url: 'https://apitest.authorize.net/rest/v1/eventtypes',
-    method: 'GET',
-    headers: headers,
+const headers = { "content-type": "application/json",
+"Authorization": "Basic " + encodedString };
 
-};
+console.log("process.env node_env: ", process.env.NODE_ENV);
+console.log("process.env hashed key: ", process.env.hashed_key);
 
-var webhooksRequestData = {
-    url: 'https://apitest.authorize.net/rest/v1/webhooks',
-    method: 'GET',
+const eventRequestData = {
+    url: config.apiEndpoint + "/eventtypes",
+    method: "GET",
     headers: headers,
 };
 
-var webhooksPostData = {
-    url: 'https://apitest.authorize.net/rest/v1/webhooks',
-    method: 'POST',
+const webhooksRequestData = {
+    url: config.apiEndpoint + "/webhooks",
+    method: "GET",
+    headers: headers,
+};
+
+const webhooksPostData = {
+    url: config.apiEndpoint + "/webhooks",
+    method: "POST",
     headers: headers,
     json: true,
 };
 
-app.use(express.static(__dirname))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-var response_event = [
-    { event_name: 'e1', webhook_id: 'a1' },
-    { event_name: 'e2', webhook_id: 'a2' },
-]
+var eventFrequencyMap = {}
+
+function parseBody(res, body) {
+    try {
+        res.send(JSON.parse(body));
+    }catch (e) {
+        var errorMessage = {};
+        if (e instanceof TypeError) {
+            errorMessage.message = "Error in Connection. Please try again";
+            res.send(errorMessage);
+        }
+        else {
+            errorMessage.message = "Error";
+            res.send(errorMessage);
+        }
+    }
+}
 
 function getEvents(res) {
-    var eventList = []
     request(eventRequestData, function (error, response, body) {
-        if (response.statusCode == 200) {
-            for (var key in JSON.parse(body)) {
-                if (!JSON.parse(body).hasOwnProperty(key)) continue;
-                var obj = JSON.parse(body)[key];
-                for (var prop in obj) {
-                    if (!obj.hasOwnProperty(prop)) continue;
-                    eventList.push(obj[prop]);
-                }
-            }
-        }
-        //else {
-        //    console.log("Error occured\n");
-        //    //console.log(JSON.parse(body));
-        //    console.log("Body: " + JSON.parse(body).status + " reason: " + JSON.parse(body).reason + " message: " + JSON.parse(body).message)
-        //}
-        res.send(eventList);
+        parseBody(res, body);
     });
-    
 }
-
 function getWebhooks(res) {
-    var webhookList = []
     request(webhooksRequestData, function (error, response, body) {
-        if (response.statusCode == 200) {
-            for (var key in JSON.parse(body)) {
-                if (!JSON.parse(body).hasOwnProperty(key)) continue;
-                var obj = JSON.parse(body)[key];
-                for (var prop in obj) {
-                    if (!obj.hasOwnProperty(prop)) continue;
-                    webhookList.push(obj[prop]);
-                }
-            }
-        }
-        //else {
-        //    console.log("Error occured\n");
-        //    //console.log(JSON.parse(body));
-        //    console.log("Body: " + JSON.parse(body).status + " reason: " + JSON.parse(body).reason + " message: " + JSON.parse(body).message)
-        //}
-        console.log("\nin get webhook\n")
-        console.log("getwebhooksfunc\n", JSON.parse(body));
-        res.send(JSON.parse(body));
-        
+        parseBody(res, body);
     });
 
 }
-app.get('/events', function (req, res) {
+app.get("/events", function (req, res) {
     getEvents(res);
- })
+ });
 
-//if (error || response == undefined || response.statusCode != 200)
-//    out = error;
-//else
-//    out = JSON.parse(body);
-//console.log(out);
-
-app.get('/webhooks', function (req, res) {
+ app.get("/webhooks", function (req, res) {
     getWebhooks(res);
 });
 
-app.post('/webhooks', function (req, res) {
-    //postWebhooks(res);
-
-    //webhooksPostData = Object.assign(webhooksPostData, req.body);
+app.post("/webhooks", function (req, res) {
     console.log("this is req.body in server.js\n");
-    //console.log(req.body);
     webhooksPostData.body = req.body;
-    console.log(webhooksPostData);
     request(webhooksPostData, function (error, response, body) {
         if (error) {
             console.log("error occured: \n", error);
@@ -122,28 +92,77 @@ app.post('/webhooks', function (req, res) {
         else {
             console.log(response.body);
             //res.sendStatus(200);
-        }
-        
+        }        
     });
 
 });
 
-app.get('/webhooks/add', function (req, res) {
+app.get("/webhooks/add", function (req, res) {
     res.send("hello");
 });
 
-app.get('/notifications', function (req, res) {
-    console.log("Got a get request at notifications page", req.body);
+app.post("/notifications", function (req, res) {
+    console.log("Got a post request at notifications page", JSON.stringify(req.body));
     //response_event.push(req.body);
-    res.send("new page");
-})
-
-app.post('/notifications', function (req, res) {
-    console.log("Got a post request at notifications page", req.body);
-    //response_event.push(req.body);
+    incrementEventOccurrence(req.body.eventType)
+    io.emit("new event", {
+        eventDetails: (req.body),
+        eventsCount: eventFrequencyMap,
+    })
     res.sendStatus(200);
 })
 
-var server = app.listen(port, function () {
-    console.log('Example app listening on port ', server.address().port)
+var increment = 1;
+
+function incrementEventOccurrence(event) {
+    console.log("occured event is in neweventgraph func:", event);
+    if (event in eventFrequencyMap) {
+        eventFrequencyMap[event]++;
+    }
+    else
+        eventFrequencyMap[event] = 1;
+    console.log("and map value is: ", eventFrequencyMap)
+}
+
+app.get("/notifications", function (req, res) {
+    console.log("\n ****inside get notifications****\n");
+    res.format({
+        // Respond to browser requests with the 404 page
+        html: function () {
+            console.log("\n inside html\n");
+            res.json({
+                error: "Page not found"
+            });
+        },
+        json: function () {
+            console.log("\n inside json\n");
+            //var eventData = [];
+            var eventPoint = {};
+            min = Math.ceil(0);
+            max = Math.floor(25);
+            var rand = Math.floor(Math.random() * (max - min)) + min;
+            eventPoint.day = new Date();
+            //eventPoint.day = increment;
+            ++increment;
+            eventPoint.event1 = rand;
+            eventPoint.event2 = rand + 5;
+            eventPoint.event3 = 0;
+            eventPoint.event4 = 0;
+            //eventData.push(eventPoint);
+            console.log("\n going to send json response in get method\n", eventPoint);
+            res.json({
+                //eventData: eventData
+                eventData: eventPoint,
+                //eventData: eventFrequencyMap,
+            });
+        }
+    })
+})
+
+app.use((req, res, next) => {
+    res.status(404).send("<h2 align=center>Page Not Found!</h2>");
+});
+
+var myServer = server.listen(config.app.port, "localhost", function () {
+    console.log("Example app listening on port ", server.address().port)
 })
