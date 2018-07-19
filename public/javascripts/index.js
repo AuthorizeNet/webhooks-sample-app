687fccf9fd81ad788b72d20d551a8d17b160140a
jaga4494 <jaga4494@gmail.com>
2018-07-18 17:53:16 -0700

    added descriptions for few functions

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,11 @@
 var socket = io();
+/**
+ * Display either Event monitoring or dashboard of graphs
+ * when the respective button is clicked
+ */
 $(() => {
     $("#includedContent").load("../mainChart.html");
+    // $("#includedContent").load("../charttest.html");
 
     $("#chart").click(() => {
         $(".eventMonitoring").css("display", "none");
@@ -13,16 +18,23 @@ $(() => {
     });
 });
 
-// Call API to populate the recent events when the either
-// page is initially loaded or refreshed
+/**
+ * Call API to populate the recent events when the either
+ * page is initially loaded or refreshed
+ */
 $.getJSON("/recentNotifications", { count: 5 }, function (results) {
     results.forEach((element) =>
                     displayEventMessage(element.eventType, element));
 });
 
+/**
+ * Calls the API in the backend to get all the webhooks
+ * created by the merchant. Creates the mapping between
+ * an event and its associated webhooks. 
+ * 
+ */
 async function getWebhookEventDetails() {
     var webhookWebhookEventDict = {};
-
     $.get("http://127.0.0.1:9000/webhooks", async function (data) {
         data.forEach((webhook) => {
             webhook.eventTypes.forEach((event) => {
@@ -31,12 +43,11 @@ async function getWebhookEventDetails() {
                 webhookWebhookEventDict[event].push(webhook.webhookId);
             })
         })
-        console.log("dict is: \n");
-        console.log(webhookWebhookEventDict);
         var webhookEventMessage = "";
         for (var key in webhookWebhookEventDict)
             webhookEventMessage += await addWebhookEventMessage(key, webhookWebhookEventDict[key]);
         printWebhookEventMessage(webhookEventMessage);
+        // Adds serial number for the table having class as "js-serial".
         addRowCount(".js-serial");
     });
 }
@@ -70,12 +81,22 @@ async function addWebhookEventMessage(key, value) {
     return webhookEventMessage;
 }
 
-// Call onNewEvent method when a new event occurs and update graph
+/**
+ * Socket listens for new event that is emitted in the server side.
+ * Calls onNewEvent method when a new event occurs and updates the graph
+ */
 socket.on("new event", (body) => {
     onNewEvent(body);
     // plotAllGraphs();
-    });
+});
 
+/**
+ * Removes the net.authorize from the eventType and formats
+ * the remaining string.
+ * Eg. Converts "net.authorize.payment.capture.created" to 
+ * PAYMENT CAPTURE CREATED
+ * @param {string} eventType 
+ */
 function extracteventNameFromEventType(eventType) {
     var eventName = eventType.split(".").slice(2)
                     .join(" ").toUpperCase();
@@ -83,7 +104,11 @@ function extracteventNameFromEventType(eventType) {
     return eventName;
 }
 
-// Converting "2018-07-21T20:52:59.2144524Z" to "2018-07-21 20:52:59"
+/**
+ * Formats the event date into only date time format
+ * Converting "2018-07-21T20:52:59.2144524Z" to "2018-07-21 20:52:59"
+ * @param {string} eDate 
+ */
 function formatEventDate(eDate) {
     var eventDate = eDate.split("T");
     eventDate[1] = eventDate[1].split(".")[0];
@@ -160,13 +185,13 @@ async function displayEventMessage(eventName, eventDetails) {
     newPanelColTimestamp.appendChild(ColTimestampTextnode);
 
     var newPanelColEventtype = document.createElement("div");
-    newPanelColEventtype.classList.add("col-md-5");
+    newPanelColEventtype.classList.add("col-md-6");
     var ColEventtypeTextnode = document.createTextNode("" + eventDetails.eventType);
     newPanelColEventtype.style.wordBreak = "break-all";
     newPanelColEventtype.appendChild(ColEventtypeTextnode);
 
     var newPanelColPayload = document.createElement("div");
-    newPanelColPayload.classList.add("col-md-5");
+    newPanelColPayload.classList.add("col-md-4");
     var ColPayloadTextnode = document.createElement("pre");
     ColPayloadTextnode.setAttribute("style", "background-color: lightgreen;");
     // Remove '{' and '}' from JSON String
@@ -249,7 +274,10 @@ function getWebhooks() {
         }
     });
 }
-
+/**
+ * Displays all webhooks in table format
+ * @param {string} webhookMessage 
+ */
 async function printWebhookMessage(webhookMessage) {
     $("#messageList").append(
         `<table class="table table-striped table-bordered table-hover table-condensed js-serial">
@@ -265,7 +293,12 @@ async function printWebhookMessage(webhookMessage) {
                 <tbody>${webhookMessage}</tbody></table>`
     );
 }
-            
+
+/**
+ * Adds serial number in the first column of table whose class name
+ * is passed.
+ * @param {string} tableAttr 
+ */
 function addRowCount(tableAttr) {
     $(tableAttr).each(function () {
         $('th:first-child, thead td:first-child', this).each(function () {
