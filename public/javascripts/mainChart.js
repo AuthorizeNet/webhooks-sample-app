33a2200ab34f3e3fee03538adbfdbbf92f2099ec
jaga4494 <jaga4494@gmail.com>
2018-07-24 16:20:07 -0700

    removed highcharts event log placeholder chart

--- public/javascripts/mainChart.js
+++ public/javascripts/mainChart.js
@@ -4,61 +4,6 @@ async function plotAllGraphs() {
     await plotGraph("refund", "chart2");
 }
 
-$.getJSON('/notifications', { name: "all" }, function (results) {
-    Highcharts.chart('chart5', {
-    chart: {
-        type: 'areaspline'
-    },
-    title: {
-        text: 'Real Time Event Monitor'
-    },
-    legend: {
-        layout: 'vertical',
-        align: 'center',
-        verticalAlign: 'bottom',
-        x: 150,
-        y: 100,
-        floating: true,
-        borderWidth: 1,
-        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
-    },
-    xAxis: {
-        categories: [
-            'Monday',
-            'Tuesday',
-            'Wednesday',
-            'Thursday',
-            'Friday',
-            'Saturday',
-            'Sunday'
-        ],
-    },
-    yAxis: {
-        title: {
-            text: 'Events Count'
-        }
-    },
-    tooltip: {
-        shared: true,
-        valueSuffix: ' units'
-    },
-    credits: {
-        enabled: false
-    },
-    plotOptions: {
-        areaspline: {
-            fillOpacity: 0.5
-        }
-    },
-    series: [{
-        name: 'event1',
-        data: [3, 4, 3, 5, 4, 10, 12]
-    }, {
-        name: 'event2',
-        data: [1, 3, 4, 3, 3, 5, 4]
-    }]
-    });
-});
 function plotGraph(eventCategory, chartId) {
     var graph = {
         name: eventCategory,
@@ -91,13 +36,13 @@ function plotGraph(eventCategory, chartId) {
         },
         yAxis: {
             title: {
-                text: 'Amount Captured'
+                text: 'Amount Captured ($)'
             }
         },
         tooltip: {
             shared: true,
             formatter: function() { 
-                return 'Date: <b>' + this.x + '</b><br>Amount: <b>'+ this.y + ' Dollars</b>';
+                return 'Date: <b>' + this.x + '</b><br>Amount: <b>'+ this.y + ' $</b>';
                 }
         },
         credits: {
