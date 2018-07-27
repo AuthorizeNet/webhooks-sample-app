3bbf830a18beeeec721b78e311b133faef18f2d7
jaga4494 <jaga4494@gmail.com>
2018-07-26 22:17:59 -0700

    moved from highcharts to chart.js for converted payment, customer, refund, fraud events

--- public/javascripts/mainChart.js
+++ public/javascripts/mainChart.js
@@ -1,64 +1,86 @@
 "use strict";
 plotAllGraphs();
-async function plotAllGraphs() {
-    await plotGraph("payment", "chart1");
-    await plotGraph("refund", "chart2");
+function plotAllGraphs() {
+    plotGraph("Payment", "chart1");
+    plotGraph("Refund", "chart2");
+    plotGraph("Customer", "chart3");
+    plotGraph("Fraud", "chart4");
 }
+var chartColorIndex = 0;
 
 function plotGraph(eventCategory, chartId) {
     var graph = {
         name: eventCategory
     };
     $.getJSON('/notifications', graph, function (results) {
-        var totalPayment = Object.values(results.paymentDetail).reduce((a,b) => a + b, 0);
-        Highcharts.chart(chartId, {
-        chart: {
-            type: 'areaspline'
-        },
-        title: {
-            text: '<b>Payment</b>',
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'center',
-            verticalAlign: 'top',
-            // x: 150,
-            // y: 100,
-            // floating: true,
-            borderWidth: 1,
-            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
-        },
-        labels: {
-                enabled: false
-            },
-        xAxis: {
-            categories: Object.keys(results.paymentDetail),
+        console.log("results in graph", (results));
+        if(Object.keys(results)) {
+                new Chart(document.getElementById(chartId).getContext("2d"), {
+                type: 'line',
+                data: {
+                    labels: (function() { 
+                        console.log("labels", (Object.keys(results)));
+                        return Object.keys(results);
+                    }()),
 
-        },
-        yAxis: {
-            title: {
-                text: 'Amount Captured ($)'
-            }
-        },
-        tooltip: {
-            shared: true,
-            formatter: function() {
-                return 'Date: <b>' + this.x + '</b><br>Amount: <b>'+ this.y + ' $</b>';
-                }
-        },
-        credits: {
-            enabled: false
-        },
-        plotOptions: {
-            areaspline: {
-                fillOpacity: 0.5
-            }
-        },
-        series: [{
-            name: 'Total Amount = ' + totalPayment,
-            data: Object.values(results.paymentDetail),
-        },]
+                    datasets: (function() {
+                                var datasetList = [];
+                                var eventDataMap = {};
+                                var nameList = Object.keys(results[Object.keys(results)[0]]);
+                                console.log("nameList in graph", (nameList));
 
-        });
+                                nameList.forEach((name) => {
+                                    eventDataMap[name] = [];
+                                });
+                                Object.keys(results).forEach((graphDate) => {
+                                   Object.keys(results[graphDate]).forEach((set) => {
+                                        eventDataMap[set].push(results[graphDate][set]);
+                                   });
+                                });
+
+                                for(var i=0; i<nameList.length; ++i) {
+                                    var colorName = colorNames[chartColorIndex % colorNames.length];
+                                    var newColor = chartColors[colorName];
+                                    ++chartColorIndex;
+
+                                    datasetList.push({
+                                        label: nameList[i],
+                                        data: eventDataMap[nameList[i]],
+                                        borderColor: newColor,
+                                        backgroundColor: newColor,
+                                    });
+                                }
+                                console.log("datasetList in graph", (datasetList[0]));
+                                return datasetList;
+                            } ())
+                },
+                options: {
+                    title: {
+                        display: true,
+                        text: eventCategory,
+                        fontSize: 20
+                    },
+                    legend:{
+                        position: "top",
+                    },
+                    scales: {
+                    yAxes : [{
+                        ticks : {
+                            min : 0,
+                            // suggestedMax: 1000,
+                            // stepSize: 100,
+                        }
+                    }]
+                    },
+                    animation: {
+                        duration: 0,
+                    },
+                    hover: {
+                        animationDuration: 0,
+                    },
+                    responsiveAnimationDuration: 0
+                },
+            });
+        }
     });
 }
